531acd93f5d47aaac6e899de0c56e210f34c9598
Kornys0301 <kornys@web.de>
2019-09-25 10:31:26 +0200

    fixed python spacing

--- main.py
+++ main.py
@@ -26,60 +26,60 @@ from time import sleep
 ##### FUNCTIONS
 
 def parseLicensePlate(img):
-	img = cv2.resize(img, (620,480) )
+    img = cv2.resize(img, (620,480) )
 
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to grey scale
-	gray = cv2.bilateralFilter(gray, 11, 17, 17) #Blur to reduce noise
-	edged = cv2.Canny(gray, 30, 200) #Perform Edge detection
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to grey scale
+    gray = cv2.bilateralFilter(gray, 11, 17, 17) #Blur to reduce noise
+    edged = cv2.Canny(gray, 30, 200) #Perform Edge detection
 
-	# find contours in the edged image, keep only the largest
-	# ones, and initialize our screen contour
-	cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
-	cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
-	screenCnt = None
+    # find contours in the edged image, keep only the largest
+    # ones, and initialize our screen contour
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+    screenCnt = None
 
-	# loop over our contours
-	for c in cnts:
-	 # approximate the contour
-	 peri = cv2.arcLength(c, True)
-	 approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+    # loop over our contours
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.018 * peri, True)
 
-	 # if our approximated contour has four points, then
-	 # we can assume that we have found our screen
-	 if len(approx) == 4:
-	  screenCnt = approx
-	  break
+        # if our approximated contour has four points, then
+        # we can assume that we have found our screen
+        if len(approx) == 4:
+            screenCnt = approx
+            break
 
-	if screenCnt is None:
-	 detected = 0
-	 print("No contour detected")
-	else:
-	 detected = 1
+    if screenCnt is None:
+        detected = 0
+        print("No contour detected")
+    else:
+        detected = 1
 
-	if detected == 1:
-	 cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 3)
+    if detected == 1:
+        cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 3)
 
-	# Masking the part other than the number plate
-	mask = np.zeros(gray.shape,np.uint8)
-	new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
-	new_image = cv2.bitwise_and(img,img,mask=mask)
+    # Masking the part other than the number plate
+    mask = np.zeros(gray.shape,np.uint8)
+    new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
+    new_image = cv2.bitwise_and(img,img,mask=mask)
 
-	# Now crop
-	(x, y) = np.where(mask == 255)
-	(topx, topy) = (np.min(x), np.min(y))
-	(bottomx, bottomy) = (np.max(x), np.max(y))
-	Cropped = gray[topx:bottomx+1, topy:bottomy+1]
+    # Now crop
+    (x, y) = np.where(mask == 255)
+    (topx, topy) = (np.min(x), np.min(y))
+    (bottomx, bottomy) = (np.max(x), np.max(y))
+    Cropped = gray[topx:bottomx+1, topy:bottomy+1]
 
-	#Read the number plate
-	text = pytesseract.image_to_string(Cropped, config='--psm 11')
-	print("Detected Number is:",text)
+    #Read the number plate
+    text = pytesseract.image_to_string(Cropped, config='--psm 11')
+    print("Detected Number is:",text)
 
-	cv2.imshow('image',img)
-	cv2.imshow('Cropped',Cropped)
+    cv2.imshow('image',img)
+    cv2.imshow('Cropped',Cropped)
 
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 ##### MAIN ENTRY
 
@@ -92,25 +92,25 @@ rawCapture = PiRGCArray(camera, size=(640, 480))
 time.sleep(0.1)
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
  
-	# show the frame
-	cv2.imshow("Frame", image)
-	
-	# wait for user key
-	key = cv2.waitKey(1) & 0xFF
-	
-	# ggf. img = cv2.imread(TMP_PICTURE_PATH,cv2.IMREAD_COLOR)
-	parseLicensePlate(image)
-	
-	# wait for user key
-	key = cv2.waitKey(1) & 0xFF
+    # show the frame
+    cv2.imshow("Frame", image)
+    
+    # wait for user key
+    key = cv2.waitKey(1) & 0xFF
+    
+    # ggf. img = cv2.imread(TMP_PICTURE_PATH,cv2.IMREAD_COLOR)
+    parseLicensePlate(image)
+    
+    # wait for user key
+    key = cv2.waitKey(1) & 0xFF
  
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
  
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
